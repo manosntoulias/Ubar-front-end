7d1b9deff164b3f0e442cdf96c82ee72260d8910
dimitris21gr <jimjohnana@yahoo.com>
2018-07-22 19:29:41 +0300

    fixed http cleartext error, gitingore new entries

--- app/src/main/java/gr/uoa/di/ecommerce/ubar/Def.java
+++ app/src/main/java/gr/uoa/di/ecommerce/ubar/Def.java
@@ -1,7 +1,7 @@
 package gr.uoa.di.ecommerce.ubar;
 
 public class Def {
-    public static final String SERVER_URL = "http://192.168.1.2:8080/";
+    public static final String SERVER_URL = "http://192.168.1.3:8080/";
     public static final String LOGIN_PATH = "user/service/login";
 
     public static final int not_found = -1;
