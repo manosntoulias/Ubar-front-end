03bb68c1790a5af100f8f39192725c24be8e8e50
manosntoulias <manosntoulias@gmail.com>
2018-07-25 16:17:17 +0300

    new first acitvity with images

---
+++ app/src/main/java/gr/uoa/di/ecommerce/ubar/Activities/MainActivity.java
@@ -0,0 +1,14 @@
+package gr.uoa.di.ecommerce.ubar.Activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import gr.uoa.di.ecommerce.ubar.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
